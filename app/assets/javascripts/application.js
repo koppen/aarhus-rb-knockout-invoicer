c30969021b5aa31c4f683eff84d567c482f150e6
Jakob Skjerning <jakob@mentalized.net>
2014-11-01 21:53:33 +0100

    Show the subtotal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,12 @@
 InvoiceLine = function() {
   this.amount = ko.observable();
   this.price = ko.observable();
+
+  var self = this;
+
+  this.subtotal = ko.computed(function() {
+    if (self.amount() && self.price()) {
+      return self.amount() * self.price();
+    }
+  })
 }

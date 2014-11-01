a8104430b5817b49f5982dd1a2ad0ff8e69b1963
Jakob Skjerning <jakob@mentalized.net>
2014-11-01 15:23:32 +0100

    Show details about invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,8 @@ class Invoice < ActiveRecord::Base
   has_many :invoice_lines
 
   accepts_nested_attributes_for :invoice_lines
+
+  def total
+    invoice_lines.collect(&:subtotal).compact.sum
+  end
 end
--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -1,3 +1,8 @@
 class InvoiceLine < ActiveRecord::Base
   belongs_to :invoice
+
+  def subtotal
+    return nil unless amount && price
+    amount * price
+  end
 end
